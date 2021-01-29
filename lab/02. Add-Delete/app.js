0ffa892a851eb671325e09a4354c5d92e3058a4a
IvanP <ivanstpv@gmail.com>
2021-01-29 15:57:38 +0200

    2,3,4 tasks

--- lab/02. Add-Delete/app.js
+++ lab/02. Add-Delete/app.js
@@ -1,7 +1,3 @@
 function addItem() {
-    const ulElement=document.getElementById('items');
-    const input=document.getElementById('newItemText').value;
-    const liToAdd=document.createElement('li');
-    liToAdd.textContent=input;
-    ulElement.appendChild(liToAdd);
+    //TODO complete after next lection
 }
--- lab/04. Mouse-Gradient/app.js
+++ lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    document.getElementById('gradient').addEventListener('mousemove', onMove);
+    const output = document.getElementById('result');
+
+    function onMove(event) {
+        //   const offsetX = event.pageX - event.target.offsetLeft;
+        //use the one above because .offsetX is considered experimental at this point in time
+        const offsetX = event.offsetX;
+        const offsetPercentX = Math.floor(offsetX * 100 / event.target.clientWidth);
+        output.textContent = `${offsetPercentX}%`;
+
+    }
 }
