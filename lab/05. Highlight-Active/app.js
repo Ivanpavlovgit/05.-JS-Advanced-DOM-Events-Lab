0e3ec9485cab7ae64db1a78c5429523d5d8e43bc
IvanP <ivanstpv@gmail.com>
2021-01-29 18:18:34 +0200

    5th task completed

--- lab/05. Highlight-Active/app.js
+++ lab/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focus() {
+
+    [...document.getElementsByTagName('input')].forEach(e => {
+        e.addEventListener('focus', onFocus);
+        e.addEventListener('blur', onBlur);
+    });
     console.log('TODO:...');
+
+
+    function onFocus(event) {
+        event.target.parentNode.className = 'focused';
+    }
+
+    function onBlur(event) {
+        event.target.parentNode.className = '';
+    }
+
 }
