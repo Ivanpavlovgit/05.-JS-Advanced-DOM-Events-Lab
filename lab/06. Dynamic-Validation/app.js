d686f69bba8d209de75cfb9dc57d2ab003fac926
IvanP <ivanstpv@gmail.com>
2021-02-01 13:09:15 +0200

    7th task completed

--- lab/06. Dynamic-Validation/app.js
+++ lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange(event) {
+        const email = event.target.value;
+        console.log('now');
+        if (
+            /^[a-z]+@[a-z]+\.[a-z]+$/.test(email)) {
+            event.target.className='';
+        }else{
+            event.target.className='error';
+        }
+    }
+
     console.log('TODO:...');
 }
